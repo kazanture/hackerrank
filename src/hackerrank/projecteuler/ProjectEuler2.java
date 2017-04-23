bd17880b60f3ca94da2ebca4ecf9f6ca1719e109
kazanture <endemre@yahoo.com>
2017-04-23 20:10:54 +0300

    ProjectEuler1

---
+++ src/hackerrank/projecteuler/ProjectEuler2.java
@@ -0,0 +1,16 @@
+package hackerrank.projecteuler;
+
+import java.util.Scanner;
+
+/**
+ * Created by kazanture on 4/23/2017.
+ */
+public class ProjectEuler2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+        }
+    }
+}
