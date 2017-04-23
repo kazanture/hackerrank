a07bf67999b6ffd424ec20145e8e77b4f4e06477
kazanture <endemre@yahoo.com>
2017-04-23 20:15:17 +0300

    ProjectEuler1

---
+++ src/hackerrank/projecteuler/ProjectEuler5.java
@@ -0,0 +1,77 @@
+package hackerrank.projecteuler;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+/**
+ * Created by kazanture on 4/23/2017.
+ */
+public class ProjectEuler5 {
+    public static long findLargestPrimeFactor(long input){
+        long largestFactor=0;
+        for(int j=2;j<=Math.sqrt(input);j++){
+           /* if(!BigInteger.valueOf(j).isProbablePrime(1))
+                continue;
+*/
+            if(input%j==0){
+                largestFactor=findLargestPrimeFactor(input/j);
+                break;
+            }
+        }
+        if(largestFactor==0)
+            largestFactor=input;
+        return largestFactor;
+    }
+    public static boolean isPalindrome(Integer number){
+        String numberAsString=number.toString();
+        StringBuffer stringBuffer=new StringBuffer(numberAsString);
+        stringBuffer.reverse();
+        return stringBuffer.toString().equals(numberAsString);
+    }
+    public static void main(String[] args) {
+        ArrayList<Integer> palindromeList=new ArrayList<Integer>();
+        Scanner in = new Scanner(System.in);
+        int testNo = in.nextInt();
+        long biggestPalindrome = 0;
+        for(int n=100;n<1000;n++){
+            for(int m=0;m<1000;m++){
+                int value=n*m;
+                if(palindromeList.indexOf(value)==-1){
+                    if(isPalindrome(value)){
+                        palindromeList.add(value);
+                    }
+                }
+            }
+        }
+        Collections.sort(palindromeList);
+        for (int i = 0; i < testNo; i++) {
+            int input = in.nextInt();
+            long minimumCommonBig=input;
+            for(int k=input-1;k>0;k--){
+                minimumCommonBig=findCommonBig(k,minimumCommonBig);
+            }
+            System.out.println(minimumCommonBig);
+        }
+    }
+    public static long findCommonBig(int x,long y){
+        for(long i=y;i<x*y;i++){
+            if(i%x==0&&i%y==0){
+                return i;
+            }
+        }
+        return x*y;
+    }
+    public static int indexOf(Integer[] a, int key) {
+        int lo = 0;
+        int hi = a.length - 1;
+        while (lo <= hi) {
+            // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return lo;
+    }
+}
