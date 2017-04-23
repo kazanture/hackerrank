dc0dec1d0446f9789665497fe63d9334cbf3e74f
kazanture <endemre@yahoo.com>
2017-04-23 20:12:02 +0300

    ProjectEuler1

--- src/hackerrank/projecteuler/ProjectEuler1.java
+++ src/hackerrank/projecteuler/ProjectEuler1.java
@@ -8,9 +8,17 @@ import java.util.*;
 public class ProjectEuler1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            int n = in.nextInt();
+        int testNo=in.nextInt();
+        for(int j=0;j<testNo;j++){
+
+            int sum=0;
+            long currentNumber=in.nextInt();
+            long n=(currentNumber-1)/3;
+            long m=(currentNumber-1)/5;
+            long k=(currentNumber-1)/15;
+            long result=3*n*(n+1)/2+5*m*(m+1)/2-15*k*(k+1)/2;
+            System.out.println(result);
+
         }
     }
 }
--- src/hackerrank/projecteuler/ProjectEuler2.java
+++ src/hackerrank/projecteuler/ProjectEuler2.java
@@ -8,9 +8,27 @@ import java.util.Scanner;
 public class ProjectEuler2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
-            long n = in.nextLong();
+        long testNo=in.nextInt();
+        for(long j=0;j<testNo;j++){
+
+            long sum=0;
+            long currentLimit=in.nextLong();
+            long prevNo=2;
+            long prevPrevNo=1;
+            long currentNo=3;
+            long total=0;
+            while(prevNo+prevPrevNo<=currentLimit){
+                currentNo=prevNo+prevPrevNo;
+                if(isEven(currentNo))
+                    total+=currentNo;
+                prevPrevNo=prevNo;
+                prevNo=currentNo;
+            }
+            System.out.println(total+2);
+
         }
+    }
+    public static boolean isEven(long number){
+        if(number%2==0) return true;else return false;
     }
 }
